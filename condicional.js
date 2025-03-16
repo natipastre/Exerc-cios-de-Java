cf4c5db64310f177fde6d9e175e049d69e54f914
Natália Baptista Pastre <113951260+natipastre@users.noreply.github.com>
2025-03-16 19:15:09 -0300

    Add files via upload

---
+++ condicional.js
@@ -0,0 +1,11 @@
+var nota = 0;
+var faltas = 9;
+
+if (nota == 10 && faltas < 10){
+    console.log("Aluno aprovado com nota máxima!")
+}else if(nota>= 5 && nota <=9.9 && faltas < 10){
+    console.log("Aluno aprovado!")
+}else{
+    console.log("Aluno reprovado!")
+}
+    
