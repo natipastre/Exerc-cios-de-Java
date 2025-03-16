86f61f4ec3788bbda92dac20e254904c1cc2a1c5
Natália Baptista Pastre <113951260+natipastre@users.noreply.github.com>
2025-03-16 19:16:54 -0300

    Add files via upload

---
+++ primeiro-script.js
@@ -0,0 +1,3 @@
+console.log("E eu sou um texto JavaScript durante o HTML")
+console.log("...e eu sou um texto criado em um arquivo JavaScript");
+console.log("Este foi o primeiro script desenvolvido por Vinícius Marin")
